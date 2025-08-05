94e2509e6b374676e17984b618c862726eb9892b
sergey <110@mobilon.ru>
2025-08-06 01:12:58 +0700

    fix

--- src/GupshupPartnerApiClient/types/index.ts
+++ src/GupshupPartnerApiClient/types/index.ts
@@ -2,6 +2,8 @@ export * from './TemplateCreateData';
 export * from './Template';
 export * from './GupshupPartnerApiClinetConfig';
 
+// Re-export specific types for better organization
+export { TemplateEditData, TemplateCreateDataButtons } from './TemplateCreateData';
 
 export interface PartnerSubscriptionDataAdd {
   url: string;
